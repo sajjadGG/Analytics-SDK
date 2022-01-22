aad2eb21a8759ce86a10f7c950b72ac339010b9e
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2022-01-22 18:41:26 +0330

    feat(front): add Panel abstract class

--- front.py
+++ front.py
@@ -20,14 +20,39 @@ class ViewComponent:
         pass
 
 
-class Dashboard(ViewComponent):
-    def __init__(self, layout) -> None:
-        self.viewCompoent = []
-        self.layout = layout
+class Panel:
+    def __init__(self) -> None:
+        self.components = []
+
+    def addComponent(self, comp: ViewComponent):
+        pass
+
+    def render() -> None:
+        pass
+
+
+class GridPanel(Panel):
+    def __init__(self, n_h, n_w) -> None:
+        self.number_rows = n_h
+        self.number_width = n_w
+
+    def addComponent(self, comp: ViewComponent):
+        self.components.append(comp)
+
+    def render() -> None:
+        pass
+
+
+# TODO: auth , profile , ...
+class Dashboard:
+    def __init__(self, panel: Panel) -> None:
+        self.panel = panel
+
+    def addComponent(self, comp: ViewComponent):
+        self.panel.components.append(comp)
 
     def render(self):
-        for v in self.viewCompoent:
-            v.render()
+        self.panel.render()
 
 
 # TODO: config of chart required data and type
